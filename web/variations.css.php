c5a9d9ce0b74a68667d1e02080f91147feadaafa
Andy Davison <andy@birdslikewires.co.uk>
2014-09-30 16:43:28 +0100

    Tidied up for plasma display.

--- web/variations.css.php
+++ web/variations.css.php
@@ -5,7 +5,7 @@ header("Content-Type: text/css");
 .studio {
 	text-align: <?php $hour = date("H");
 
-if (($hour > 9) && ($hour < 16)) { 
+if (($hour >= 9) && ($hour < 17)) { 
 	echo "left;"; 
 } else { 
 	echo "right;"; 
